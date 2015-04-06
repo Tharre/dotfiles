acec706874ad8a59ba75b5784848f9bca1dce6e6
Tharre <tharre3@gmail.com>
2015-04-06 16:47:41 +0200

    Quote paths to honor whitespaces in them

--- install.sh
+++ install.sh
@@ -6,19 +6,19 @@ root=$(pwd)
 git submodule update --init
 
 # [zsh]
-ln -sf $root/shell/_zshrc $HOME/.zshrc
-ln -sfT $root/shell/_oh-my-zsh $HOME/.oh-my-zsh
+ln -sf "$root/shell/_zshrc" "$HOME/.zshrc"
+ln -sfT "$root/shell/_oh-my-zsh" "$HOME/.oh-my-zsh"
 
 # [emacs]
-ln -sfT $root/dotEmacs $HOME/.emacs.d
+ln -sfT "$root/dotEmacs" "$HOME/.emacs.d"
 
 # [vim]
-ln -sf $root/shell/_vimrc $HOME/.vimrc
+ln -sf "$root/shell/_vimrc" "$HOME/.vimrc"
 
 # [git]
-ln -sf $root/shell/_gitconfig $HOME/.gitconfig
+ln -sf "$root/shell/_gitconfig" "$HOME/.gitconfig"
 
 # [tmux]
-ln -sf $root/shell/_tmux.conf $HOME/.tmux.conf
+ln -sf "$root/shell/_tmux.conf" "$HOME/.tmux.conf"
 
 echo "Finished installing dotfiles"
