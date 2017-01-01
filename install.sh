db44f7f327c9a0cbf31c2571fa5ad0600a1c8bb5
Tharre <tharre3@gmail.com>
2017-01-01 02:25:02 +0100

    Don't create my directory layout in install.sh

--- install.sh
+++ install.sh
@@ -4,29 +4,11 @@ if [ "$#" -ne 1 ]; then
 	# make sure all submodules are there
 	git submodule update --init --recursive
 
-	# create directories
-	mkdir -p ~/bin
-	mkdir -p ~/etc
-	mkdir -p ~/share
-	mkdir -p ~/media
-	mkdir -p ~/media/games
-	mkdir -p ~/media/music
-	mkdir -p ~/media/pictures
-	mkdir -p ~/media/videos
-	mkdir -p ~/var
-	mkdir -p ~/var/cache
-	mkdir -p ~/var/downloads
-	mkdir -p ~/var/log
-	mkdir -p ~/var/run
-	mkdir -p ~/var/tmp
-	mkdir -p ~/var/vim
-	mkdir -p ~/var/VMs
+	# ensure mail directory exists
 	mkdir -p ~/.mail
 
 	find . -maxdepth 1 ! -path . ! -name .git ! -name .gitmodules \
 		! -name .gitignore ! -name .updated -name '.*' -exec "$0" {} \;
-
-	echo "Finished installing dotfiles"
 else
 	canonical=$(echo $1 | sed "s|^\./||")
 	target="$HOME/$canonical"
