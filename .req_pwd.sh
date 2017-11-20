26fbdfa1339de88b7c11103803c1d4f48e3bbd6b
Tharre <tharre3@gmail.com>
2017-11-20 14:56:13 +0100

    Kill gpg-agent with killall(1) again ...
    gpgconf unfortunately proved to not work quite as expected either.

--- .req_pwd.sh
+++ .req_pwd.sh
@@ -5,7 +5,7 @@
 pass "$1" && exit
 
 # maybe gpg-agent is acting up, retry after restarting it
-gpgconf --kill gpg-agent >&2
+killall gpg-agent >&2
 pass "$1" && exit
 
 # if pass fails, request password manually
